6ad316531763ab552a4fb6445e5b1e2a0a3ff7a2
PlumSnow <1141138286@qq.com>
2024-04-08 15:29:17 +0800

    C端新增模块：地址簿管理

---
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -0,0 +1,10 @@
+package com.sky.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sky.entity.AddressBook;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface AddressBookMapper extends BaseMapper<AddressBook> {
+}
