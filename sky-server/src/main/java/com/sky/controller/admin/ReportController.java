8b07cf3b06f94693d1a1779700ea8ec480a6acb5
PlumSnow <1141138286@qq.com>
2024-04-10 17:15:18 +0800

    B端数据统计模块新增功能：排名销量TOP10统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -1,12 +1,15 @@
 package com.sky.controller.admin;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.sky.entity.OrderDetail;
 import com.sky.entity.Orders;
 import com.sky.entity.User;
 import com.sky.result.Result;
+import com.sky.service.OrdersDetailService;
 import com.sky.service.OrdersService;
 import com.sky.service.UserService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -16,7 +19,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +26,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @RequestMapping("admin/report")
@@ -37,6 +37,8 @@ public class ReportController {
     private OrdersService ordersService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private OrdersDetailService ordersDetailService;
 
     /**
      * 营业额统计
@@ -136,6 +138,7 @@ public class ReportController {
 
 
     @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
     public Result<OrderReportVO> ordersStatistics(
             @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate begin,
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
@@ -200,4 +203,72 @@ public class ReportController {
 
         return Result.success(orderReportVO);
     }
+    
+    @GetMapping("/top10")
+    @ApiOperation("销售排行")
+    public Result<SalesTop10ReportVO> salesTop10Statistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+
+        List<LocalDate> dataList = new ArrayList<>();
+
+        LocalDate l = begin;
+        while(!l.isEqual(end)){
+            l = l.plusDays(1);
+            dataList.add(l);
+        }
+
+        String dateJoin = StringUtils.join(dataList, ",");
+
+        LambdaQueryWrapper<Orders> lambdaQueryWrapper4Orders = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper4Orders.between(Orders::getOrderTime, begin, end);
+        List<Orders> ordersList = ordersService.list(lambdaQueryWrapper4Orders);
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(Orders orders : ordersList){
+            if(Objects.equals(orders.getStatus(), Orders.COMPLETED)){
+                LambdaQueryWrapper<OrderDetail> lamdaQueryWrapper4OrderDetail = new LambdaQueryWrapper<>();
+                lamdaQueryWrapper4OrderDetail.eq(OrderDetail::getOrderId, orders.getId());
+                List<OrderDetail> orderDetailList = ordersDetailService.list(lamdaQueryWrapper4OrderDetail);
+
+                for(OrderDetail orderDetail : orderDetailList){
+                    map.put(orderDetail.getName(), orderDetail.getNumber());
+                }
+
+            }
+
+        }
+
+        //对map根据数量进行降序排序，仅保留前十项
+        List<String> top10 = new ArrayList<>();
+        List<Integer> top10Count = new ArrayList<>();
+        String maxKey = "";
+        Integer max = 0;
+        while(!map.isEmpty() && top10.size() < 10) {
+
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+
+                if (entry.getValue() > max) {
+                    max = entry.getValue();
+                    maxKey = entry.getKey();
+                }
+
+            }
+
+            top10.add(maxKey);
+            top10Count.add(max);
+            map.remove(maxKey);
+            max = 0;
+            maxKey = "";
+        }
+
+        String top10Join = StringUtils.join(top10, ",");
+        String top10CountJoin = StringUtils.join(top10Count, ",");
+
+
+        return Result.success(new SalesTop10ReportVO(top10Join, top10CountJoin));
+    }
+
 }
+
